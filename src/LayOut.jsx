97a68b4ed31c9da7266bad2470bc6a7be8c0aa37
mdsiaofficial <mdsiaofficial@gmail.com>
2025-07-27 22:41:04 +0600

    feat: job pipeline section added

--- src/LayOut.jsx
+++ src/LayOut.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Global/Header";
 import NavBar from "./components/Global/NavBar";
 import Footer from "./components/Global/Footer";
 import GoToTop from "./components/Global/GoToTop";
-import SideProfile from "./components/Global/SideProfile";
+// import SideProfile from "./components/Global/SideProfile";
 
 export default function Layout() {
   return (
