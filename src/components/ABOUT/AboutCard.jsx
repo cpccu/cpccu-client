bc6bbbcaf9b0ff8133c0e1928a62a319500a1ced
anisurrahmanfarazi17 <anisurrahmanfarazi@gmail.com>
2026-05-21 19:44:24 +0600

    fix: update AboutCard and ProfileID components to use fullName and roles for better user data display

--- src/components/ABOUT/AboutCard.jsx
+++ src/components/ABOUT/AboutCard.jsx
@@ -12,17 +12,17 @@ export default function AboutCard({ Data }) {
           height={400}
           width={400}
           className="h-full w-full object-cover group-hover:scale-105 trans"
-          src={Data?.img || DefaultAvater}
-          alt={Data?.name || "Profile Picture"}
-        />
-      </section>
-      <section className="flex flex-col items-center text-center justify-center gap-1">
-        <h1 className="text-2xl font-semibold capitalize text-p-text">
-          {Data?.name}
-        </h1>
-        <p className={`italic capitalize text-xl ${Data?.type && "font-bold"}`}>
-          {Data?.position}
-        </p>
+	          src={Data?.avatar || Data?.img || DefaultAvater}
+	          alt={Data?.fullName || Data?.name || "Profile Picture"}
+	        />
+	      </section>
+	      <section className="flex flex-col items-center text-center justify-center gap-1">
+	        <h1 className="text-2xl font-semibold capitalize text-p-text">
+	          {Data?.fullName || Data?.name}
+	        </h1>
+	        <p className={`italic capitalize text-xl ${Data?.type && "font-bold"}`}>
+	          {Data?.roles?.role || Data?.position}
+	        </p>
 
         <Link
           href={`mailto:${Data?.email}`}
